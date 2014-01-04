83edad67382aec086a441e56e7daa8bffd9c8e11
Sven Ruppert <sven.ruppert@rapidpm.org>
2014-01-04 11:22:37 +0100

    final with testfx 3.0.0

--- src/test/java/junit/org/rapidpm/demo/jaxenter/blog0010/SimpleButtonTest.java
+++ src/test/java/junit/org/rapidpm/demo/jaxenter/blog0010/SimpleButtonTest.java
@@ -1,17 +1,14 @@
 package junit.org.rapidpm.demo.jaxenter.blog0010;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.loadui.testfx.GuiTest;
 import org.loadui.testfx.categories.TestFX;
-import static org.loadui.testfx.Matchers.hasLabel;
 
 import static org.loadui.testfx.Assertions.verifyThat;
+import static org.loadui.testfx.Matchers.hasText;
 
 /**
  * Created by ts40 on 20.12.13.
@@ -21,30 +18,22 @@ public class SimpleButtonTest extends GuiTest {
 
     @Override
     protected Parent getRootNode() {
-        final VBox vBox = new VBox();
         final Button btn = new Button();
         btn.setId("btn");
-        btn.setText("Click me");
-        vBox.getChildren().add(btn);
-        return vBox;
+        btn.setText("Hello World");
+        btn.setOnAction((actionEvent)-> btn.setText( "was clicked" ));
+        return btn;
     }
 
     @Test
     public void shouldClickButton()
     {
         final Button button = find( "#btn" );
-        button.setOnAction( new EventHandler<ActionEvent>()
-        {
-            @Override
-            public void handle( ActionEvent actionEvent )
-            {
-                button.setText( "Was clicked" );
-            }
-        } );
 
         click(button);
 
-        verifyThat( "#btn", hasLabel("Was clicked") );
+//        verifyThat( "#btn", hasText("Was clicked") );
+        verifyThat( "#btn", hasText("was clicked") );
     }
 
 }
